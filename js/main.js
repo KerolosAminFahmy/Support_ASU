3e3f1e327dbc62ade55086cb5a6e8631fc765890
ATN20222 <antonabdalla30@gmail.com>
2023-10-23 23:35:56 +0300

    FinishHome

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var nav = document.querySelector('header');
+
+window.onscroll = function() {
+
+var scrollPosition = window.scrollY;
+var threshold = 10;
+nav.style.position = "sticky";
+nav.style.background = "#262626";
+if (scrollPosition > threshold) {
+        nav.style.boxShadow = 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px';
+}else{
+    nav.style.background = "#000";
+    nav.style.boxShadow = 'none';
+}
+}
